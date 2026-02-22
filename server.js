972b6967a634c73de840d25651eeb3b2082d37f3
Raúl Muñoz <128885714+rmv777@users.noreply.github.com>
2026-02-22 16:18:19 +0100

    Add CORS support for specific origin

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({
+    origin: "https://rmv777.github.io"
+}));
+
+app.use(express.json());
+
+
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
